4ea8ed42b996276ec84006ece39b297ff583a84d
Ashwin0422 <ashwinashwin17016@gmail.com>
2025-09-22 18:25:44 +0530

    frontend api changes

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL =  'http://localhost:5000';
+const API_URL =  'https://contact-book-1-dem9.onrender.com';
 
 const api = axios.create({
     baseURL: API_URL,
